9b5db281215575ad8f3c5c69ad99d4c50bf57817
Rodrigo Cotting <cottingfontes@hotmail.com>
2023-04-06 17:27:30 -0300

    Part 1 - Impl 'Inscritos'
    - Criado método para chamada do DAO com a nova @query select para trazer somente as atividades referente aos eventos do usuario logado na tela de AtividadesV2.xhtml
    - Implementação da primeira parte do funcionamento de inscrições no Evento.

---
+++ src/br/com/pluri/eventor/business/UsuarioAtividadeSB.java
@@ -0,0 +1,30 @@
+package br.com.pluri.eventor.business;
+
+import java.util.List;
+
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.etechoracio.common.business.BaseSB;
+import br.com.pluri.eventor.dao.UsuarioAtividadeDAO;
+import br.com.pluri.eventor.model.UsuarioAtividade;
+
+public class UsuarioAtividadeSB extends BaseSB {
+	
+	private UsuarioAtividadeDAO usuarioAtividadeDAO;
+	
+	protected void postConstructImpl() {
+		usuarioAtividadeDAO = getDAO(UsuarioAtividadeDAO.class);
+	}
+	
+	@Transactional(propagation=Propagation.REQUIRED)
+	public void insert(UsuarioAtividade usuAtiv) {
+		usuarioAtividadeDAO.save(usuAtiv);
+	}
+	
+	@Transactional(propagation=Propagation.NOT_SUPPORTED)
+	public List<UsuarioAtividade> findIncritosNoEventoByUsuarioLogado(Long idUsuario) {
+		return usuarioAtividadeDAO.findIncritosNoEventoByUsuarioLogado(idUsuario);
+	}
+
+}
